662eade1d822dae5db74e51c5bbece536dc21224
HJSUNG <38451225+HJSUNG@users.noreply.github.com>
2018-09-07 11:25:27 +0900

    Commit test 9.7

--- src/mainPackage/Main.java
+++ src/mainPackage/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test Commit");
+		System.out.println("Design of Data Structure");
+		System.out.println("20130895 Sung Ho-Joon");
 	}
 
 }
